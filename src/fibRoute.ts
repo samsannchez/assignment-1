78576a8243599c301bde2cb302237afbe092ea99
samsannchez <69266761+samsannchez@users.noreply.github.com>
2024-01-16 12:57:16 -0500

    Added checker for negative fibonacci

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -6,7 +6,12 @@ export default (req: Request, res: Response): void => {
   const { num } = req.params;
 
   const parsedNum: number = parseInt(num, 10);
-  
+
+  if (isNaN(parsedNum) || parsedNum < 0) {
+    res.status(400).send(`fibonacci(${num}) is undefined`);
+    return;
+  }
+
   const fibN = fibonacci(parseInt(num));
   let result = `fibonacci(${num}) is ${fibN}`;
 
